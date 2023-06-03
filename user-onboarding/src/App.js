9a0daf1492875a662b1a1fda461a24a92f29c2e5
Ben Johnson <monk.sound.design@gmail.com>
2023-06-03 12:42:58 -0400

    schema

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import * as yup from 'yup'
 import Form from './Form';
 
 
@@ -10,6 +12,30 @@ function App() {
     password: "",
     terms: false
   }
+
+  const [users, setUSers] = useState([])
+  const [formValues, setFormValues] = useState(initialFormValues)
+
+  const schema = yup.object({
+    fName: yup
+      .string()
+      .required("First Name is Required")
+      .min(2, "First Name Must Contain 2 or More Characters")
+      .max(30, "First Name Must Be 30 Characters or Less"),
+    lName: yup
+      .string()
+      .required("Last Name is Required")
+      .min(2, "Last Name Must Contain 2 or More Characters")
+      .max(30, "Last Name Must Be 30 Characters or Less"),
+    email: yup
+      .string()
+      .required("Email is Required"),
+    password: yup
+      .string()
+      .required("Please Enter Your Password"),
+    terms: yup
+      .boolean()
+  })
 
   return (
     <div className="App">
